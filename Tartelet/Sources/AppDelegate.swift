418baed0755ff907410b694b4f4b593183a98ce3
Simon Støvring <mail@simonbs.dk>
2023-05-08 22:40:56 +0200

    Replaces VirtualMachineFleetService with VirtualMachineFleet (#7)

--- Tartelet/Sources/AppDelegate.swift
+++ Tartelet/Sources/AppDelegate.swift
@@ -8,6 +8,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ notification: Notification) {
         CompositionRoot.editorService.stop()
-        CompositionRoot.fleetService.stop()
+        CompositionRoot.fleet.stop()
     }
 }
