21cd40b2997008b75a960875dc86b8c7b4037fa0
Simon Støvring <mail@simonbs.dk>
2023-05-10 13:31:55 +0200

    Adds setting to automatically start virtual machines on app launch

---
+++ Packages/VirtualMachineAutomaticLauncher/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VirtualMachineAutomaticLauncher",
+    platforms: [.macOS(.v13)],
+    products: [
+        .library(name: "VirtualMachineAutomaticLauncher", targets: ["VirtualMachineAutomaticLauncher"])
+    ],
+    dependencies: [
+        .package(path: "../Logging"),
+        .package(path: "../Settings"),
+        .package(path: "../VirtualMachine")
+    ],
+    targets: [
+        .target(name: "VirtualMachineAutomaticLauncher", dependencies: [
+            .product(name: "LogConsumer", package: "Logging"),
+            .product(name: "SettingsStore", package: "Settings"),
+            .product(name: "VirtualMachineFleet", package: "VirtualMachine")
+        ]),
+        .testTarget(name: "VirtualMachineAutomaticLauncherTests", dependencies: [
+            .product(name: "LogConsumer", package: "Logging"),
+            .product(name: "SettingsStore", package: "Settings"),
+            "VirtualMachineAutomaticLauncher",
+            .product(name: "VirtualMachineFleet", package: "VirtualMachine")
+        ])
+    ]
+)
