16f137fa97910c04f2b8af3e2ea00fdf7bb4946d
Simon Støvring <mail@simonbs.dk>
2023-06-15 14:31:38 +0200

    Adds more logging (#32)

--- Packages/TartVirtualMachine/Sources/EphemeralTartVirtualMachine/EphemeralTartVirtualMachine.swift
+++ Packages/TartVirtualMachine/Sources/EphemeralTartVirtualMachine/EphemeralTartVirtualMachine.swift
@@ -1,5 +1,7 @@
 import Combine
 import Foundation
+import LogHelpers
+import OSLog
 import Tart
 import TartDirectoryHelpers
 import VirtualMachine
@@ -17,6 +19,7 @@ public final class EphemeralTartVirtualMachine: VirtualMachine {
     private let resourcesDirectoryURL: URL
     private let onCleanup: CleanupHandler
     private var runTask: Task<(), Error>?
+    private let logger = Logger(category: "EphemeralTartVirtualMachine")
 
     public init(
         tart: Tart,
@@ -36,8 +39,13 @@ public final class EphemeralTartVirtualMachine: VirtualMachine {
         defer {
             onCleanup()
         }
+        let sourceVMName = sourceVMName
+        let destinationVMName = destinationVMName
+        logger.info("Clone Tart image named \(sourceVMName, privacy: .public) to \(destinationVMName, privacy: .public)...")
         try await tart.clone(sourceName: sourceVMName, newName: destinationVMName)
+        logger.info("Run Tart image named \(destinationVMName, privacy: .public)...")
         try await tart.run(name: destinationVMName, mounting: [.resources(at: resourcesDirectoryURL)])
+        logger.info("Delete Tart image named \(destinationVMName, privacy: .public)...")
         try await tart.delete(name: destinationVMName)
         onCleanup()
     }
