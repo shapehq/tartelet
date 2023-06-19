92940587b1ab1044cb124dccfe9087f20f5fd9a4
Simon Støvring <mail@simonbs.dk>
2023-06-19 16:55:58 +0200

    Adds GitHubPrivateKeyPicker (#35)

--- Packages/Settings/Sources/SettingsUI/Internal/GitHubSettings/GitHubSettingsView.swift
+++ Packages/Settings/Sources/SettingsUI/Internal/GitHubSettings/GitHubSettingsView.swift
@@ -16,41 +16,10 @@ struct GitHubSettingsView: View {
                 .disabled(!viewModel.isSettingsEnabled)
             TextField(L10n.Settings.Github.appId, text: $viewModel.appId)
                 .disabled(!viewModel.isSettingsEnabled)
-            LabeledContent {
-                VStack {
-                    HStack {
-                        if let privateKeyName = viewModel.privateKeyName {
-                            Text(privateKeyName)
-                                .lineLimit(1)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        } else {
-                            Text(L10n.Settings.Github.PrivateKey.placeholder)
-                                .foregroundColor(.secondary)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        Button {
-                            if let fileURL = presentOpenPanel() {
-                                Task {
-                                    await viewModel.storePrivateKey(at: fileURL)
-                                }
-                            }
-                        } label: {
-                            Text(L10n.Settings.Github.PrivateKey.selectFile)
-                        }.disabled(!viewModel.isSettingsEnabled)
-                    }
-                    Text(L10n.Settings.Github.PrivateKey.scopes)
-                        .multilineTextAlignment(.center)
-                        .lineSpacing(4)
-                        .padding(8)
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(.secondary)
-                        .background {
-                            RoundedRectangle(cornerRadius: 12)
-                                .stroke(.separator, lineWidth: 1)
-                        }
+            GitHubPrivateKeyPicker(filename: $viewModel.privateKeyName, isEnabled: viewModel.isSettingsEnabled) { fileURL in
+                Task {
+                    await viewModel.storePrivateKey(at: fileURL)
                 }
-            } label: {
-                Text(L10n.Settings.Github.privateKey)
             }
             Button {
                 viewModel.openCreateApp()
@@ -62,17 +31,5 @@ struct GitHubSettingsView: View {
         .task {
             await viewModel.loadCredentials()
         }
-    }
-}
-
-private extension GitHubSettingsView {
-    func presentOpenPanel() -> URL? {
-        let openPanel = NSOpenPanel()
-        openPanel.allowedContentTypes = [.x509Certificate]
-        openPanel.allowsMultipleSelection = false
-        openPanel.canChooseDirectories = false
-        openPanel.canChooseFiles = true
-        let response = openPanel.runModal()
-        return response == .OK ? openPanel.url : nil
     }
 }
